fb83b4a4b467c8fc93dc882594ed0fd2ec7806ed
neilburt <neil.burt@comcast.net>
2021-07-02 17:11:27 -0500

    Made adjustments to the format of markdown generation.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ inquirer
 {
   name: 'license',
   type: 'list',
-  message: "Select the licenses that this application is covered under.",
+  message: "Select the license that this application is covered under.",
   choices: ["Apache", "Eclipse", "GNU", "MIT", "Mozilla", "Unlicense"]
 },
 {
@@ -54,43 +54,43 @@ inquirer
   let table = `## Table of Contents\n`;
 
   if(responses.install){
-    table += `* [Installation](#installation)\n`
+    table += `- ### [Installation](#installation)\n`
   }
   if(responses.usage){
-    table += `* [Usage](#usage)\n`
+    table += `- ### [Usage](#usage)\n`
   }
   if(responses.license !== ""){
-    table += `* [Licenses](#licenses)\n`
+    table += `- ### [Licenses](#licenses)\n`
   }
   if(responses.credits){
-    table += `* [Contributing](#contributing)\n`
+    table += `- ### [Contributing](#contributing)\n`
   }
   if(responses.tests){
-    table += `* [Tests](#tests)\n`
+    table += `- ### [Tests](#tests)\n`
   }
   if(responses.questions){
-    table += `* [Questions](#questions)\n`
+    table += `- ### [Questions](#questions)\n`
   }
 
   const readme =
-`# ${responses.title}\n
-![license badge](https://img.shields.io/badge/license-${responses.license}-orange)\n
-## Description\n
-${responses.description}\n
-${table}\n
-## Installation\n
-${responses.install}\n
-## Usage\n
-${responses.usage}\n
-## License\n
-This application is covered under the ${responses.license} license.\n
-${responses.license}\n
-## Contributing\n
-${responses.credits}\n
-## Tests\n
-${responses.tests}\n
-## Questions\n
-Visit my [GitHub profile](https://github.com/${responses.github}).\n
+`# ${responses.title}  \n
+![license badge](https://img.shields.io/badge/license-${responses.license}-orange)  \n
+## Description  \n
+${responses.description}  \n
+${table}  \n
+## Installation  \n
+${responses.install}  \n
+## Usage  \n
+${responses.usage}  \n
+## License  \n
+This application is covered under the ${responses.license} license.  \n
+## Contributing  \n
+${responses.credits}  \n
+## Tests  \n
+${responses.tests}  \n
+## Questions  \n
+If you have any additional questions:  \n
+Visit my [GitHub profile](https://github.com/${responses.github}).  \n
 Or you can [email me](mailto:${responses.email}).`
 
   fs.writeFile("README.md", readme, (error) => {
